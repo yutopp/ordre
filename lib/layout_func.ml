7efebb97686a8f0bf5838ab2e292f0cb0391d171
yutopp <yutopp@gmail.com>
2021-01-22 02:26:21 +0900

    Add KnotSet

--- lib/layout_func.ml
+++ lib/layout_func.ml
@@ -9,6 +9,21 @@
 open! Base
 module KnotMap = Map.M (Int)
 
+module KnotSet = struct
+  include Set.M (Int)
+
+  let at_minus_kv g x : int =
+    let seq = Set.to_sequence ~order:`Decreasing ~less_or_equal_to:x g in
+    Sequence.hd_exn seq
+
+  let at_plus g x : int =
+    let seq =
+      Set.to_sequence ~order:`Increasing ~greater_or_equal_to:x g
+      |> Sequence.filter ~f:(fun k -> k > x)
+    in
+    Sequence.hd seq |> Option.value ~default:Int.max_value
+end
+
 type elem_t = {
   (* layout expression *)
   l : Expr.t;
@@ -27,7 +42,11 @@ let empty () = Map.empty (module Int)
 
 let add g x ~l ~s ~a ~b = Map.add_exn g ~key:x ~data:{ l; s; a; b }
 
-let knots g = Map.keys g
+let knots g =
+  Map.to_sequence g
+  |> Sequence.map ~f:(fun (k, _v) -> k)
+  |> Sequence.to_list
+  |> Set.of_list (module Int)
 
 let to_alist g = Map.to_alist g
 
--- test/layout_func_test.ml
+++ test/layout_func_test.ml
@@ -51,6 +51,19 @@ let test_at_minus_kv () =
     (2, { l = Helper.u; s = 10; a = 20; b = 30 })
     (at_minus_kv g 3)
 
+let test_at_minus_kv_sets () =
+  let open Ordre.Layout_func in
+  let g = empty () in
+  let g = add g 0 ~l:Helper.u ~s:1 ~a:2 ~b:3 in
+  let g = add g 2 ~l:Helper.u ~s:10 ~a:20 ~b:30 in
+  let g = add g 10 ~l:Helper.u ~s:100 ~a:200 ~b:300 in
+  let ks = knots g in
+
+  Alcotest.(check int) "at 0" 0 (KnotSet.at_minus_kv ks 0);
+  Alcotest.(check int) "at 1" 0 (KnotSet.at_minus_kv ks 1);
+  Alcotest.(check int) "at 2" 2 (KnotSet.at_minus_kv ks 2);
+  Alcotest.(check int) "at 3" 2 (KnotSet.at_minus_kv ks 3)
+
 let test_at_plus () =
   let open Ordre.Layout_func in
   let g = empty () in
@@ -63,12 +76,33 @@ let test_at_plus () =
   Alcotest.(check int) "at 2" 10 (at_plus g 2);
   Alcotest.(check int) "at 10" Int.max_value (at_plus g 10)
 
+let test_at_plus_sets () =
+  let open Ordre.Layout_func in
+  let g = empty () in
+  let g = add g 0 ~l:Helper.u ~s:1 ~a:2 ~b:3 in
+  let g = add g 2 ~l:Helper.u ~s:10 ~a:20 ~b:30 in
+  let g = add g 10 ~l:Helper.u ~s:100 ~a:200 ~b:300 in
+  let ks = knots g in
+
+  Alcotest.(check int) "at 0" 2 (KnotSet.at_plus ks 0);
+  Alcotest.(check int) "at 1" 2 (KnotSet.at_plus ks 1);
+  Alcotest.(check int) "at 2" 10 (KnotSet.at_plus ks 2);
+  Alcotest.(check int) "at 10" Int.max_value (KnotSet.at_plus ks 10)
+
 let () =
   let open Alcotest in
   run "Layout_func"
     [
       ("empty", [ test_case "0" `Quick test_empty ]);
       ("add", [ test_case "0" `Quick test_add ]);
-      ("at_minus_kv", [ test_case "0" `Quick test_at_minus_kv ]);
-      ("at_plus", [ test_case "0" `Quick test_at_plus ]);
+      ( "at_minus_kv",
+        [
+          test_case "map" `Quick test_at_minus_kv;
+          test_case "set" `Quick test_at_minus_kv_sets;
+        ] );
+      ( "at_plus",
+        [
+          test_case "map" `Quick test_at_plus;
+          test_case "set" `Quick test_at_plus_sets;
+        ] );
     ]
